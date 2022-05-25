d82767745b151d72a13a54a1e7de0c47854748b4
uprain1116 <74394318+uprain1116@users.noreply.github.com>
2022-05-25 15:17:21 +0900

    Update ProfilePic.js

--- my-app/src/components/profilePage/ProfilePic.js
+++ my-app/src/components/profilePage/ProfilePic.js
@@ -1,12 +1,22 @@
 import "./profile.css";
 function ProfilePic(props){
+
+    const addImage = () =>{
+        console.log('add image');
+    }
+
+    const rmvImage = () => {
+        console.log('remove image');
+    }
+
+
     return(
         <div className = "profiletype">
             <div className="profileContentTitle">Profile photo</div>
             <div id = "profileMainContainer">
                 <div id = "profilePicContain"> <img id = "profilePic" src = "logo512.png" alt = "pic"/> </div>
-                <div> <button id = "addImg_button"> Add New Image </button> </div>
-                <div> <button id = "rmvImg_button"> Remove Image </button> </div>
+                <div> <button type = "button" id = "addImg_button" onClick={addImage}> Choose new image </button> </div>
+                <div> <button type = "button" id = "rmvImg_button" onClick={rmvImage}> Remove image </button> </div>
             </div>
         </div>
     );
